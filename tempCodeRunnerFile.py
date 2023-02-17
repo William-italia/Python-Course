fe5ae33ce88e6405443de302718137bcf37e7a48
william-italia <williamitalia70@outlook.com>
2023-02-17 20:41:16 -0300

    deleted: TempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-print(1, "Luiz \"Otavio\"")
-print(2, 'Luiz "Otario"')
-print('Explícito', 'é', 'melhor " do que implícito')
-print(type(float(2)), type("Luiz 'carvalo'"))
-
-print(str(11) + 'a')
-print(bool(10 > 10.30))
